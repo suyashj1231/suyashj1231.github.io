eecd640c7eae3a2bf0aad0d0a5bb299a5b85872c
suyashj1231 <149845903+suyashj1231@users.noreply.github.com>
2024-09-09 01:51:42 +0530

    About section completed

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./assets/components/About";
 import Hero from "./assets/components/hero";
 import NavBar from "./assets/components/NavBar";
 
@@ -5,13 +6,13 @@ export default function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
 
-      <div className="flixed top-0 -z-10 h-full w-full"></div>
-
+      <div className="fixed top-0 -z-10 h-full w-full">
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-
+      </div>
       <div className="container mx-auto px-8">
       <NavBar/>
       <Hero/>
+      <About/>
       </div>
       
     </div>
