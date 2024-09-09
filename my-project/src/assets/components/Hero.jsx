7dd8226b44f51acf238eddc75efed61cedf18c0c
suyashj1231 <149845903+suyashj1231@users.noreply.github.com>
2024-09-09 21:17:27 +0530

    ADDING MOTION: Testing working

--- my-project/src/assets/components/Hero.jsx
+++ my-project/src/assets/components/Hero.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 import { HERO_CONTENT } from "../../constants/index.js"
 import profilePic from "../kevinRushProfile.png"
+
+
 const Hero = () => {
   return( <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
     <div className='flex flex-wrap'>
         <div className='w-full lg:w-1/2'>
             <div className='flex flex-col items-center lg:items-start'>
-                <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Suyash 
-                <span className='text-neutral-500'> Jain</span></h1>
-                <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'> Full Stack Developer</span>
+                <motion.h1 animate={{ x: 100 }} className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Suyash
+                <span className='text-neutral-500'> Jain</span>
+                </motion.h1>
+                <motion.span whileHover={{ scale: 1.2 }}
+                  whileTap={{ scale: 1.1 }}
+                  drag="x"
+                  dragConstraints={{ left: -100, right: 100 }} className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'> Full Stack Developer</motion.span>
                 <p className='my-2 max-w-xl py-6 font-light tracking-tight'>{HERO_CONTENT}</p>
             </div>
         </div>
