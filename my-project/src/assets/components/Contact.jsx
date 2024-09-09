60daa8e577871fa866e380caa8c1f4797aebb749
suyashj1231 <149845903+suyashj1231@users.noreply.github.com>
2024-09-10 02:00:35 +0530

    DOING THE 1st hosting!

--- my-project/src/assets/components/Contact.jsx
+++ my-project/src/assets/components/Contact.jsx
@@ -10,7 +10,8 @@ const Contact = () => {
         whileInView={{opacity:1, y:0}}
         initial={{opacity:0, y:-100}}
         transition={{duration:0.5}}        
-        className='my-10 text-center text-4xl'>Get in Touch</motion.h2>
+        className='my-10 text-center text-4xl'>Get
+        <span className='text-neutral-500'> in Touch</span></motion.h2>
         <div className='text-center tracking-tighter'>
             <motion.p
             whileInView={{opacity:1, x:0}}
