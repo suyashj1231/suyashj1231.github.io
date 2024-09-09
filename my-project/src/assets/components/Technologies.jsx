f8a3e3ac791fe7c001365032cba5d482dd784106
suyashj1231 <149845903+suyashj1231@users.noreply.github.com>
2024-09-09 23:52:27 +0530

    Technologies Section Motion Completed

--- my-project/src/assets/components/Technologies.jsx
+++ my-project/src/assets/components/Technologies.jsx
@@ -1,46 +1,93 @@
 import React from 'react'
 import {RiReactjsLine} from 'react-icons/ri'
-import {TbBrandNextjs} from 'react-icons/tb'
-import {SiMongodb} from 'react-icons/si'
-import {DiRedis} from 'react-icons/di'
 import {FaNodeJs} from 'react-icons/fa'
-import {BiLogoPostgresql} from 'react-icons/bi'
-
-// mine
 import { FaPython } from 'react-icons/fa'; 
 import { SiAndroidstudio } from 'react-icons/si';
 import { SiRust } from 'react-icons/si';
 import { SiCplusplus } from 'react-icons/si';
 import { SiFlutter } from 'react-icons/si';
+import { animate, motion } from 'framer-motion'
+
+const iconVariants = (duration) => ({
+    initial: { y: -15 },
+  animate: {
+    y: [0, -15],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
+  }
+});
+
 
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-800 pb-24'>
-        <h2 className='my-20 text-center text-4xl'> Technologies</h2>
-        <div className='flex flex-wrap items-center justify-center gap-4'>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        <motion.h2
+        whileInView={{opacity:1, y:0}}
+        initial={{opacity:0, y:-100}}
+        transition={{duration:1.5}}
+        className='my-20 text-center text-4xl'> Technologies</motion.h2>
+        <motion.div
+        whileInView={{opacity:1, x:0}}
+        initial={{opacity:0, x:-100}}
+        transition={{duration:1.5}}
+        className='flex flex-wrap items-center justify-center gap-4'>
+
+            <motion.div
+            variants={iconVariants(2)}
+            initial='initial'
+            animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaNodeJs className='text-7xl text-green-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiCplusplus className='text-7xl text-blue-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaPython className='text-7xl text-yellow-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiRust className='text-7xl text-orange-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiAndroidstudio className='text-7xl text-green-700'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiFlutter className='text-7xl text-blue-400'/>
-            </div>
             
-        </div>
+            </motion.div>
+            <motion.div
+             variants={iconVariants(3)}
+             initial='initial'
+             animate='animate'
+             className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <FaNodeJs className='text-7xl text-green-500'/>
+            </motion.div>
+            <motion.div
+             variants={iconVariants(1.3)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <SiCplusplus className='text-7xl text-blue-500'/>
+            </motion.div>
+            <motion.div
+             variants={iconVariants(3)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <FaPython className='text-7xl text-yellow-500'/>
+            </motion.div>
+            <motion.div
+             variants={iconVariants(1.5)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <SiRust className='text-7xl text-orange-500'/>
+            </motion.div>
+            <motion.div
+             variants={iconVariants(2)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <SiAndroidstudio className='text-7xl text-green-700'/>
+            </motion.div>
+            <motion.div
+             variants={iconVariants(2.8)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <SiFlutter className='text-7xl text-blue-400'/>
+            </motion.div>
+            
+        </motion.div>
     </div>
   )
 }
