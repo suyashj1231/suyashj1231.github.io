4bba3174504b700422f1e0c358959166f6346db9
suyashj1231 <149845903+suyashj1231@users.noreply.github.com>
2024-09-10 00:18:15 +0530

    Completed Contact

--- my-project/src/assets/components/Contact.jsx
+++ my-project/src/assets/components/Contact.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import { CONTACT } from '../../constants'
+import {motion} from 'framer-motion'
+
 
 const Contact = () => {
   return (
     <div className='border-b border-neutral-900 pb-20'>
-        <h2 className='my-10 text-center text-4xl'>Get in Touch</h2>
+        <motion.h2
+        whileInView={{opacity:1, y:0}}
+        initial={{opacity:0, y:-100}}
+        transition={{duration:0.5}}        
+        className='my-10 text-center text-4xl'>Get in Touch</motion.h2>
         <div className='text-center tracking-tighter'>
-            <p className='my-4'>{CONTACT.address}</p>
-            <p className='my-4'>{CONTACT.phoneNo}</p>
-            <p className='my-4'>
+            <motion.p
+            whileInView={{opacity:1, x:0}}
+            initial={{opacity:0, x:100}}
+            transition={{duration:1}}
+            className='my-4'>{CONTACT.address}</motion.p>
+            <motion.p
+            whileInView={{opacity:1, x:0}}
+            initial={{opacity:0, x:-100}}
+            transition={{duration:1}}
+            className='my-4'>{CONTACT.phoneNo}</motion.p>
+            <motion.p
+             className='my-4'>
                 <a href={`mailto:${CONTACT.email}`} className='border-b'>{CONTACT.email}</a>
-            </p>
-            <p className='my-4'>
+            </motion.p>
+            <motion.p className=''>
                 <a href={CONTACT.LinkedIn} className='border-b'>LinkedIn</a>
-            </p>
+            </motion.p>
         </div>
     </div>
   )
