6b417094731a5b2a2e94ba928bfc8c8222a94510
suyashj1231 <149845903+suyashj1231@users.noreply.github.com>
2026-01-21 03:42:20 -0800

    Fixed Images

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -1,61 +1,61 @@
 import React from 'react'
-import aboutImg from "../about.jpg"
+
 import aboutImg1 from "../about1.png"
 import { ABOUT_TEXT } from '../../constants'
 import { ABOUT_EDUCATION } from '../../constants'
 import { motion } from 'framer-motion'
 
 const About = () => {
-  return (
-  <div className='border-b border-neutral-900 pb-4'>
-    <motion.h2
-    whileInView={{opacity:1, y:0}}
-    initial={{opacity:0, y:-100}}
-    transition={{duration:1.5}}
-    className='my-20 text-center text-4xl'>
-        About
-        <span className='text-neutral-500'> Me</span>
-        </motion.h2>
-        <div className='flex flex-wrap'>
-            <motion.div
-            whileInView={{opacity:1, x:0}}
-            initial={{opacity:0, x:-100}}
-            transition={{duration:0.5}}
-            className='w-full lg:w-1/2 lg:p-8'>
-                <div className= "flex items-center justify-center">
-                    <img className=" mt-5 rounded-2xl"src= {aboutImg1} alt="About" />
-                </div>
-            </motion.div>
-            <motion.div
-            whileInView={{opacity:1, x:0}}
-            initial={{opacity:0, x:100}} 
-            transition={{duration:0.5}}
-            className='w-full lg:w-1/2'>
-                <div className='flex justify-center lg:justify-start'>
-                    <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
-                </div>
-                <div className='flex justify-center lg:justify-start'>
-                <div className='my-2 max-w-xl py-4 text-center lg:text-left'>
-                    <h3 className='text-2xl font-semibold'>Education</h3>
-                    <div>
-                    {ABOUT_EDUCATION.map((education, index) => (
-                        <div key={index} className="mt-2">
-                            <p className="mb-2">
-                                <span className="text-sm text-neutral-500">{education.year}</span>
-                                <span className=" px-2 py-1 text-medium font-medium text-purple-100">{education.major}</span>
-                            </p>
-                            <p className="text-sm text-cyan-400">{education.location}</p>
-                            <div className="mb-4"></div>
-                        </div>
-                    ))}
+    return (
+        <div className='border-b border-neutral-900 pb-4'>
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1.5 }}
+                className='my-20 text-center text-4xl'>
+                About
+                <span className='text-neutral-500'> Me</span>
+            </motion.h2>
+            <div className='flex flex-wrap'>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 0.5 }}
+                    className='w-full lg:w-1/2 lg:p-8'>
+                    <div className="flex items-center justify-center">
+                        <img className=" mt-5 rounded-2xl" src={aboutImg1} alt="About" />
                     </div>
-                    {/* <p>{ABOUT_EDUCATION}</p> */}
-            </div>
-            </div>
-            {/* UCI end */}
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 0.5 }}
+                    className='w-full lg:w-1/2'>
+                    <div className='flex justify-center lg:justify-start'>
+                        <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
+                    </div>
+                    <div className='flex justify-center lg:justify-start'>
+                        <div className='my-2 max-w-xl py-4 text-center lg:text-left'>
+                            <h3 className='text-2xl font-semibold'>Education</h3>
+                            <div>
+                                {ABOUT_EDUCATION.map((education, index) => (
+                                    <div key={index} className="mt-2">
+                                        <p className="mb-2">
+                                            <span className="text-sm text-neutral-500">{education.year}</span>
+                                            <span className=" px-2 py-1 text-medium font-medium text-purple-100">{education.major}</span>
+                                        </p>
+                                        <p className="text-sm text-cyan-400">{education.location}</p>
+                                        <div className="mb-4"></div>
+                                    </div>
+                                ))}
+                            </div>
+                            {/* <p>{ABOUT_EDUCATION}</p> */}
+                        </div>
+                    </div>
+                    {/* UCI end */}
 
-        </motion.div>
-        </div>
+                </motion.div>
+            </div>
         </div>)
 }
 
--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -1,48 +1,47 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { HERO_CONTENT } from "../../constants/index.js"
-import profilePic from "../kevinRushProfile.png"
-import profilePic1 from "../profile2.png"
+import profilePic1 from "../Main-profile-no-bg.png"
 
 const container = (delay) => ({
-  hidden: { x:-100, opacity:0 },
-  visible: {x:0, opacity:1, transition:{duration:0.5, delay: delay}}
+  hidden: { x: -100, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } }
 })
 
 const Hero = () => {
-return (<div className='border-b border-neutral-900 pb-4 lg:mb-35'>
-  <div className='flex flex-wrap'>
-    <div className='w-full lg:w-1/2'>
-      <div className='flex flex-col items-center lg:items-start'>
-        <motion.h1
-        variants={container(0)}
-        initial='hidden'
-        animate='visible'
-        className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Suyash
-          <span className='text-neutral-500'> Jain</span>
-        </motion.h1>
-        <motion.span
-         variants={container(0.5)}
-         initial='hidden'
-         animate='visible'
-         className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight  text-transparent'>
-         Full Stack Developer</motion.span>
-        <motion.p
-         variants={container(1)}
-         initial='hidden'
-         animate='visible'
-         className='my-2 max-w-xl py-6 font-light tracking-tight'>{HERO_CONTENT}</motion.p>
+  return (<div className='border-b border-neutral-900 pb-4 lg:mb-35'>
+    <div className='flex flex-wrap'>
+      <div className='w-full lg:w-1/2'>
+        <div className='flex flex-col items-center lg:items-start'>
+          <motion.h1
+            variants={container(0)}
+            initial='hidden'
+            animate='visible'
+            className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Suyash
+            <span className='text-neutral-500'> Jain</span>
+          </motion.h1>
+          <motion.span
+            variants={container(0.5)}
+            initial='hidden'
+            animate='visible'
+            className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight  text-transparent'>
+            Full Stack Developer</motion.span>
+          <motion.p
+            variants={container(1)}
+            initial='hidden'
+            animate='visible'
+            className='my-2 max-w-xl py-6 font-light tracking-tight'>{HERO_CONTENT}</motion.p>
+        </div>
       </div>
+      <div className='w-full lg:w-1/2 lg:p-8'>
+        <motion.img
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0., opacity: 1 }}
+          transition={{ duration: 1, delay: 1.2 }}
+          src={profilePic1} alt="Profile pic"
+          className='object-cover rounded-lg' /></div>
     </div>
-    <div className='w-full lg:w-1/2 lg:p-8'>
-      <motion.img 
-       initial= {{x:100, opacity:0}}
-       animate= {{x:0., opacity:1}}
-       transition={{duration:1, delay:1.2}}
-       src={profilePic1} alt="Profile pic" 
-       className='object-cover rounded-lg'/></div>
-  </div>
-</div>)
+  </div>)
 }
 
 export default Hero
