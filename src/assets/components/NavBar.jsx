c205204529c2e16e28bd36a48b29c73d84b44cdf
suyashj1231 <149845903+suyashj1231@users.noreply.github.com>
2026-01-21 03:51:14 -0800

    Fixed navbar issues

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -1,34 +1,35 @@
 import logo from "../logosjw.png";
-import {FaLinkedin, FaGithub, FaInstagram,FaFilePdf } from 'react-icons/fa'
+import { FaLinkedin, FaGithub, FaInstagram, FaFilePdf } from 'react-icons/fa'
 import React from 'react'
 import { CONTACT } from '../../constants';
 import { motion } from 'framer-motion';
+import resume from "./Resume_Suyash_v01212026.pdf";
 
 
 const iconLogo = (duration) => ({
   initial: { y: -10 },
-animate: {
-  y: [10, -10],
-  transition: {
-    duration: duration,
-    ease: "linear",
-    repeat: Infinity,
-    repeatType: "reverse"
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
   }
-}
 });
 
 const NavBar = () => {
   return <nav className='mb-20 flex items-center justify-between py-6'>
-    <motion.div 
-    variants={iconLogo(8)}
-    initial='initial'
-    animate='animate'
-    className='flex flex-shrink-0 items-center'>
-        <img src={logo} className='mx-2 w-20' alt="logo" />
+    <motion.div
+      variants={iconLogo(8)}
+      initial='initial'
+      animate='animate'
+      className='flex flex-shrink-0 items-center'>
+      <img src={logo} className='mx-2 w-20' alt="logo" />
     </motion.div>
     <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-      <a href="https://drive.google.com/file/d/1a2J7jExMEPUaV929Lmji6XyI7DKY7ph1/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+      <a href={resume} target="_blank" rel="noopener noreferrer">
         <FaFilePdf />
       </a>
       <a href={CONTACT.LinkedIn} target="_blank" rel="noopener noreferrer">
